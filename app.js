022033e59f5b49fb780c29b95baf10245c0651a4
Vincent Leombruno <vincent@Vincents-MacBook-Air.local>
2016-01-19 16:12:08 -0500

    2 hours later

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+
+
+
+app.get("/", function(request, response, next){
+	response.send("some text");
+	next();
+})
+
+app.listen(3000, function(){
+	console.log("listening on port 3000");
+})
+
+app.use(function(request, response, next){
+	console.log(request.method + " " + request.path + " " + response.statusCode);
+})
