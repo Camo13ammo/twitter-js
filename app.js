196bd44dbf499b138abef2eb3b6d71a7695741a5
Vincent Leombruno <vincent@Vincents-MacBook-Air.local>
2016-01-19 17:24:02 -0500

    completed up to rendering

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var app = express();
+var swig = require ('swig');
 
 
+// in some file that is in the root directory of our application
+var locals = {
+    title: 'An Example',
+    people: [{
+        name: 'Gandalf',
+    }, {
+        name: 'Frodo'
+    }, {
+        name: 'Hermione'
+    }]
+};
+
+
+swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+    console.log(output);
+});
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false });
 
 
 app.get("/", function(request, response, next){
-	response.send("some text and then some more text");
+	response.render('index', locals);
 	next();
 })
 
@@ -16,3 +40,7 @@ app.listen(3000, function(){
 app.use(function(request, response, next){
 	console.log(request.method + " " + request.path + " " + response.statusCode);
 })
+
+
+
+
